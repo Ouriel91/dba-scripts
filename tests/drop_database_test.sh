5f4a5d13cca9f9f1004c9c8c870f29fb272b1ec2
Ouriel91 <orieloh91@gmail.com>
2024-05-19 15:38:45 +0000

    its seems works but maybe cause side effects

--- tests/drop_database_test.sh
+++ tests/drop_database_test.sh
@@ -6,45 +6,36 @@ export ORACLE_HOME=/opt/oracle/product/21c/dbhome_1
 export DATE=$(date +%y-%m-%d_%H%M%S)
 
 #do backup
-./../scripts/backup.sh
+#./../scripts/backup.sh
 
 #drop database
-rman target / <<EOF
-STARTUP FORCE MOUNT;
-ALTER SYSTEM ENABLE RESTRICTED SESSION;
-DROP DATABASE NOPROMPT;
-quit;
-EOF
+#rman target / <<EOF
+#STARTUP FORCE MOUNT;
+#ALTER SYSTEM ENABLE RESTRICTED SESSION;
+#DROP DATABASE NOPROMPT;
+#quit;
+#EOF
 
-# Iterate over files in the current directory
+selection=''
 con_files=()
 for file in $(ls /opt/oracle/homes/OraDB21Home1/dbs/c-*); do
     con_files+=("$file")
 done
 
-for index in "${!con_files[@]}"; do
-    echo "$index: ${con_files[index]}"
-done
-
-read -p "Enter the index of the element you want to access: " index
-selection=''
-# Validate the index
-if [[ $index =~ ^[0-9]+$ && $index -ge 0 && $index -lt ${#con_files[@]} ]]; then
-    # Access the element at the specified index
-    selection="${con_files[index]}"
-else
-    echo "Invalid index. Please enter a valid index within the range of the array."
-fi
-echo $selection
+selection=${con_files[-1]}
 
 rman target / <<EOF
 startup force mount;
+quit;
+EOF
+
+rman target / <<EOF
 restore controlfile from '$selection';
 RESTORE DATABASE;
 RECOVER DATABASE;
 RESTORE DATABASE VALIDATE;
 VALIDATE DATABASE;
 RECOVER CORRUPTION LIST;
-ALTER DATABASE OPEN RESETLOGS;
+ALTER DATABASE OPEN; 
 quit;
 EOF
